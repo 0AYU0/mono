d88d92f800d30a51a889b299e4d6c0db65c52dfe
Arjun Sampath <arjundsampath@gmail.com>
2023-03-17 22:56:42 -0700

    finished typecheck pattern

--- proj_root/src/typecheck.rs
+++ proj_root/src/typecheck.rs
@@ -6,32 +6,43 @@ use crate::types::{*};
 pub fn concretify(td: &TypeDefinition, t: &T) -> T{
   match t {
     Named(s) => {
-      match td.get(s) {
-        Some(k) => concretify(td, &k),
-        _ => (*t).clone(),
-      }
+      td.get(s)
+        .map(|k| concretify(td, &k))
+        .unwrap_or_else(|| *t)
     },
-    _ => (*t).clone()
+    _ => *t
   }
 }
 
-pub fn typecheck_pattern(td: &TypeDefinition, p: &PatternT, t: &T) -> Vec<(String, T)>{
-  match (p, concretify(td, t)) {
+pub fn typecheck_pattern(td: &TypeDefinition, p: &PatternT, t: &T) -> Option<Vec<(String, T)>>{
+  match (p, &concretify(td, t)) {
     (PatternT::Tuple(p1), T::Tuple(p2)) => {
-      let merges: Vec<(Vec<(String, T)>)> = p1.iter().zip(p2.iter()).map(|(p, t)| typecheck_pattern(td, p, t)).collect();
-      merges.concat()
+      let merges: Vec<Vec<(String, T)>> = p1.iter().zip(p2.iter()).map(|(p, t)| typecheck_pattern(td, p, t).unwrap()).collect();
+      Some(merges.concat())
     },
     (PatternT::Ctor (i,p), T::Variant (variants)) => {
-      for (s, t) in variants.iter() {
-        if(s == i) {
-          return typecheck_pattern(td, p, t);
-        }
+      let filtered: Option<&T> = variants.iter().filter_map(|(s, t)| if s == i { Some(t) } else { None }).next();
+      match filtered {
+        Some(ty) => typecheck_pattern(td, p, ty),
+        _ => None
       }
-      Vec::new()
-    }
+    },
     (PatternT::Ctor (i,p), T::Named (x)) => {
-
-    }
+      let t =  match td.get(x) {
+        Some(Variant(k)) => {
+          let filtered: Option<&T> = k.iter().filter_map(|(s, t)| if s == i { Some(t) } else { None }).next();
+          match filtered {
+            Some(ty) => typecheck_pattern(td, p, ty),
+            _ => None
+          }
+        },
+        _ => None,
+      };
+      t
+    },
+    (PatternT::Var (i), _) => Some(vec![(*i, *t)]),
+    (PatternT::Wildcard, _) => Some(Vec::new()),
+    _ => None
   }
 }
 
@@ -39,30 +50,19 @@ pub fn typecheck(ec: &EvalContext, tc: &TypeContext, td: &TypeDefinition, vc: &V
   match e {
     ExprT::Wildcard => None,
     ExprT::Unctor(i, _) => {
-        match vc.get(&i) {
-          Some((t1, _)) => Some((*t1).clone()),
-          _ => None,
-        }
+      vc.get(&i).map(|(t1, _)| *t1)
     },
     ExprT::Var(s) => {
-      match tc.get(&s) {
-        Some(t1) => Some((*t1).clone()),
-        _ => None,
-      }
+      tc.get(&s).map(|t1| *t1)
     },
     ExprT::App(e1, e2) => {
-      let expr_t1 = typecheck(ec, tc, td, vc, *e1);
-      match expr_t1 {
-        Some(t) => {
-          let t1 = concretify(td, &t);
-          if let Arrow(_, t12) = t1 {
-            Some(*t12)
-          } else {
-            None
-          }
-        },
-        _ => None,
-      }
+      typecheck(ec, tc, td, vc, *e1)
+        .map(|t| {
+            match concretify(td, &t) {
+                Arrow(_, t12) => Some(*t12),
+                _ => None,
+            }
+        })
     },
     ExprT::Func(param, e) => {
       let name = param.p_name;
