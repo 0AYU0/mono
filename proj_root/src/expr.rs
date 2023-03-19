7c96ba782066cdc85fbe419b695f48d486df73db
Arjun Sampath <arjundsampath@gmail.com>
2023-03-19 14:42:34 -0700

    finished eval

--- proj_root/src/expr.rs
+++ proj_root/src/expr.rs
@@ -1,5 +1,6 @@
 use egg::LanguageChildren;
 use std::cmp::{min, max};
+use std::collections::HashMap;
 
 use crate::types::T;
 
@@ -148,6 +149,22 @@ pub fn is_ctor_exp(t: ExprT) -> bool {
   }
 }
 
+pub fn unitv() -> Value {
+  return Value::TupleV(Vec::new());
+}
+
+pub fn truev_() -> Value {
+  return Value::CtorV("True".to_string(), Box::new(unitv()));
+}
+
+pub fn falsev_() -> Value {
+  return Value::CtorV("False".to_string(), Box::new(unitv()));
+}
+
+fn value_of_bool(b:bool) -> Value {
+	if b { truev_()} else { falsev_()}
+}
+
 pub fn is_unctor_exp(t: ExprT) -> bool {
 	match t { 
     ExprT::Unctor(_, _) => true,
@@ -190,6 +207,32 @@ pub fn exp_of_value(v:Value) -> Option<ExprT> {
     },
     Value::Bot => None
   }
+}
+
+fn matches_pattern_and_extractions(p: &PatternT, v: &Value) -> Option<Vec<(String, Value)>> {
+  match (p, v) {
+      (PatternT::Tuple(ps), Value::TupleV(vs)) => Some(ps.iter().zip(vs.iter()).map(|(p, v)| matches_pattern_and_extractions(p, v).unwrap()).flatten().collect()),
+      (PatternT::Ctor(i, p), Value::CtorV(i_, v)) => {
+          if i == i_ {
+              matches_pattern_and_extractions(p, v)
+          } else {
+              None
+          }
+      },
+      (PatternT::Var(i), v) => {
+          if let Value::WildcardV = v {
+              None
+          } else {
+              Some(vec![(i.to_string(), v.clone())])
+          }
+      },
+      (PatternT::Wildcard, _) => Some(vec![]),
+      _ => None
+  }
+}
+
+fn replace_holes(eval_context: HashMap<String, ExprT>, e: ExprT) -> ExprT {
+  eval_context.iter().fold(e.clone(), |acc, (i, e)| replace(i, e.clone(), acc))
 }
 
 fn replace(i: &str, e_with: ExprT, e: ExprT) -> ExprT {
@@ -241,6 +284,7 @@ fn replace(i: &str, e_with: ExprT, e: ExprT) -> ExprT {
 }
 
 pub fn evaluate(e: ExprT) -> Option<Value> {
+    let e_cop = e.clone();
     match e {
       ExprT::Wildcard => Some(Value::WildcardV),
       ExprT::Var(i) => None,
@@ -257,6 +301,65 @@ pub fn evaluate(e: ExprT) -> Option<Value> {
           _ => None
         }
       },
+      ExprT::Eq(b, e1, e2) => {
+        let v1 = evaluate(*e1)?;
+        let v2 = evaluate(*e2)?;
+        let eq = v1 == v2;
+        let res = if b { eq } else { !eq };
+        Some(value_of_bool(res))
+      },
+      ExprT::Func(a, v) => Some(Value::FuncV(a.clone(), *v.clone())),
+      ExprT::Ctor(i, e) => {
+        let v = evaluate(*e)?;
+        Some(Value::CtorV(i, Box::new(v)))
+      },
+      ExprT::Match(e, branches) => {
+        let v = evaluate(*e)?;
+        let mut bindings_branchexp_opt = vec![];
+        for (p, branch_e) in branches.iter() {
+            let mut binding_opt = None;
+            match matches_pattern_and_extractions(&p, &v) {
+                Some(bindings)=> binding_opt = Some((bindings, branch_e)),
+                _ => {}
+            };
+            bindings_branchexp_opt.push(binding_opt);
+        }
+        let (bindings, branch_e) = bindings_branchexp_opt
+            .iter()
+            .find_map(|x| x.clone())?;
+        let mut eval_context = HashMap::new();
+        for (i, v) in bindings.iter() {
+            assert_ne!(*v, Value::WildcardV);
+            let exp = exp_of_value(v.clone())?;
+            eval_context.insert(i.clone(), exp);
+        }
+        evaluate(replace_holes(eval_context, branch_e.clone()))
+    },
+    ExprT::Fix (i,_,e2) => {
+      evaluate(replace(&i, e_cop, *e2))
+    },
+    ExprT::Tuple(es) => {
+      let vs = es.iter().map(|s| evaluate(s.clone()).unwrap()).collect();
+      Some(Value::TupleV(vs))
+    },
+    ExprT::Proj(i, e) => {
+      let v = evaluate(*e)?;
+      match v {
+        Value::WildcardV => Some(Value::WildcardV),
+        Value::TupleV(vs) => Some(vs[i as usize].clone()),
+        _ => None
+      }
+    },
+    ExprT::Unctor(i, e) => {
+      let v = evaluate(*e)?;
+      match v {
+        Value::CtorV(i1, v1) => {
+          assert_eq!(i, i1);
+          Some(*v1)
+        },
+        _ => None
+      }
+    }
       _ => todo!()
     }
 }
